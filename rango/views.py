e06ba4b9a0b5b901f0434ecbe5290383e85d9e96
Dan Barton <danbarton2000@gmail.com>
2017-06-23 10:38:26 +0100

    Server side cookies

--- rango/views.py
+++ rango/views.py
@@ -13,39 +13,37 @@ def about(request):
 
 def index(request):
 
-    category_list = Category.objects.all()
+    category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {'categories': category_list, 'pages': page_list}
 
-    visits = int(request.COOKIES.get('visits', '1'))
-
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
     reset_last_visit_time = False
-    response = render(request, 'rango/index.html', context_dict)
-    # Does the cookie last_visit exist?
-    if 'last_visit' in request.COOKIES:
-        last_visit = request.COOKIES['last_visit']
-        # Cast the value to a Python date/time object.
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
         last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
 
-        # If it's been more than one second since last visit
-        if (datetime.now() - last_visit_time).seconds > 1:
+        # Every time the user refreshes the page, update the visit counter
+        if (datetime.now() - last_visit_time).seconds > 0:
             visits = visits + 1
-            print visits
-            # ...and flag that the cookie last visit needs to be updated
             reset_last_visit_time = True
     else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
         reset_last_visit_time = True
 
-    context_dict['visits'] = visits
-    response = render(request, 'rango/index.html', context_dict)
-
     if reset_last_visit_time:
-        response.set_cookie('last_visit', datetime.now())
-        response.set_cookie('visits', visits)
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
 
-    # Return response back to the user, updating any cookies that need changed.
+
+    response = render(request,'rango/index.html', context_dict)
+
     return response
-
 
 def category(request, category_name_slug):
 
